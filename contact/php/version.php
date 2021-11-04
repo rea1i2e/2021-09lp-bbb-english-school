51e3eb70a2fb71b14e7fc65e5428e5f74ba5e750
yoshiaki <afmaar128@gmail.com>
2021-11-04 11:38:59 +0900

    add /contact

---
+++ contact/php/version.php
@@ -0,0 +1,13 @@
+<?php
+
+
+/*--------------------------------------------------------------------------
+	
+	Script Name    : Responsive Mailform
+	Author         : FIRSTSTEP - Motohiro Tani
+	Author URL     : https://www.1-firststep.com
+	Create Date    : 2014/03/25
+	Version        : 8.0
+	Last Update    : 2021/09/20
+	
+--------------------------------------------------------------------------*/
